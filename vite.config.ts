deee617a24a3f51e43096748d9bf0549b91681c0
Aurélien Grimaud <a.grimaud@involt.io>
2024-05-07 18:38:13 +0200

    chore: only build components and styles

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(() => ({
     react(),
     tsConfigPaths(),
     dts({
-      include: ["src"],
+      include: ["src/components", "src/styles"],
     }),
   ],
   build: {
