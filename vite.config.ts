5146592610b6cc36eae8bb7c8a6215b26ade4a40
Aurélien Grimaud <a.grimaud@involt.io>
2024-05-08 15:46:46 +0200

    build: update the way the dist is organized

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,65 @@
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import tsConfigPaths from "vite-tsconfig-paths";
-import * as packageJson from "./package.json";
+import path, { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { globSync } from 'glob'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import dts from 'vite-plugin-dts'
 
-export default defineConfig(() => ({
+export default defineConfig({
   plugins: [
     react(),
-    tsConfigPaths(),
+    libInjectCss(),
     dts({
-      insertTypesEntry: true,
-    }),
+      exclude: [
+        './src/stories',
+        './src/test',
+        './src/components/**/*.test.tsx',
+        './src/App.css',
+        './src/App.tsx',
+        './src/index.css',
+        './src/main.tsx',
+      ]
+    })
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: "figmug-ui",
-      formats: ["es", "umd"],
-      fileName: (format) => `figmug-ui.${format}.js`,
-    },
-    optimizeDeps: {
-      exclude: Object.keys(packageJson.peerDependencies),
+      entry: resolve(__dirname, './src/main.ts'),
+      formats: ['es'],
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        globSync(['./src/components/**/*.tsx', './src/index.ts'])
+          .filter(file => !/\.test\.tsx$/.test(file))
+          .map((file) => {
+            const entryName = path.relative(
+              'src',
+              file.slice(0, file.length - path.extname(file).length)
+            )
+            const entryUrl = fileURLToPath(new URL(file, import.meta.url))
+            return [entryName, entryUrl]
+          })
+      ),
       output: {
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name][extname]',
         globals: {
           react: 'React',
-          'react-dom': 'ReactDOM',
+          'react-dom': 'React-dom',
+          'react/jsx-runtime': 'react/jsx-runtime',
         },
       },
     },
   },
-}));
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+    coverage: {
+      include: ['./src/components'],
+      exclude: ['./src/stories'],
+    },
+  },
+})
