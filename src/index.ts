e2f8c3add2af75b76119faec0d4a32ef021ce7bb
Aurélien Grimaud <a.grimaud@involt.io>
2024-05-09 02:27:03 +0200

    export new components

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+import texts from './styles/texts.module.scss'
+
 export { Button } from './components/actions/button/Button'
 export { Tabs } from './components/actions/tabs/Tabs'
 export { Icon } from './components/icon/Icon'
 export { Select } from './components/inputs/select/Select'
+export { Input } from './components/inputs/input/Input'
+export { Dropdown } from './components/inputs/dropdown/Dropdown'
+export { Message } from './components/dialogs/messages/Message'
+export { List } from './components/lists/list/List'
+export { FormItem } from './components/slots/form-item/FormItem'
 export { Chip } from './components/tags/chip/Chip'
+export { texts }
