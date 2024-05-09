3488dd6651ac433ca8b6ff91d3348a9469d3478e
Aurélien Grimaud <a.grimaud@involt.io>
2024-05-09 02:05:56 +0200

    add: FormItem

---
+++ src/components/slots/form-item/FormItem.tsx
@@ -0,0 +1,57 @@
+import { Chip } from '../../tags/chip/Chip';
+import { Message } from '../../dialogs/messages/Message';
+import texts from '../../../styles/texts.module.scss';
+import './form-item.scss';
+
+export interface FormItemProps {
+  id: string;
+  label?: string;
+  helper?: {
+    type: 'INFO' | 'ERROR';
+    message: string;
+  };
+  shouldFill: boolean;
+  isBlocked: boolean;
+  isNew: boolean;
+  children: React.ReactNode;
+}
+
+export const FormItem = (props: FormItemProps) => {
+  const { id, label, helper, shouldFill, isBlocked, isNew, children } = props;
+
+  return (
+    <>
+      <div
+        className={[
+          'form-item',
+          shouldFill ? 'form-item--fill' : null,
+          isBlocked ? 'form-item--blocked' : null,
+        ]
+          .filter((n) => n)
+          .join(' ')}
+      >
+        {label !== undefined ? (
+          <label className={`type ${texts.type}`} htmlFor={id}>
+            {label}
+          </label>
+        ) : null}
+        {children}
+        {isNew ? <Chip>New</Chip> : null}
+      </div>
+      {helper !== undefined ? (
+        <div className="form-item__helper">
+          <Message
+            icon={helper.type === 'INFO' ? 'info' : 'warning'}
+            messages={[helper.message]}
+          />
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+FormItem.defaultProps = {
+  shouldFill: true,
+  isBlocked: false,
+  isNew: false,
+};
