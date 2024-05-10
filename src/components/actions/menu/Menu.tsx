6b8646000d57b425e2afd67313dd4701d8cf13e7
Aurélien Grimaud <a.grimaud@involt.io>
2024-05-10 22:47:31 +0200

    add: Menu component

---
+++ src/components/actions/menu/Menu.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import type { DropdownOption } from '../../../types/list.types';
+import type { IconList } from '../../../types/icon.types';
+import { Button } from '../button/Button';
+import { List } from '../../lists/list/List';
+import './menu.scss';
+
+export interface MenuProps {
+  id?: string;
+  type: 'ICON' | 'PRIMARY';
+  label?: string;
+  icon?: IconList;
+  options: Array<DropdownOption>;
+  selected?: string;
+  state?: 'DEFAULT' | 'DISABLED' | 'LOADING'
+  alignment?: 'TOP_RIGHT' | 'TOP_LEFT' | 'BOTTOM_RIGHT' | 'BOTTOM_LEFT';
+}
+
+export interface MenuStates {
+  isMenuOpen: boolean;
+}
+
+export class Menu extends React.Component<MenuProps, MenuStates> {
+  static defaultProps: Partial<MenuProps> = {
+    type: 'ICON',
+    options: [],
+    state: 'DEFAULT',
+    alignment: 'BOTTOM_LEFT',
+  };
+
+  constructor(props: MenuProps) {
+    super(props);
+    this.state = {
+      isMenuOpen: false,
+    };
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentDidMount = () =>
+    document.addEventListener('mousedown', this.handleClickOutside);
+
+  componentWillUnmount = () =>
+    document.removeEventListener('mousedown', this.handleClickOutside);
+
+  handleClickOutside = (e: Event) => {
+    const { id } = this.props;
+
+    if (!(e.target as HTMLElement).closest(`#${id}`))
+      this.setState({
+        isMenuOpen: false,
+      });
+  };
+
+  // Direct actions
+  closeMenu = (action: void) => {
+    this.setState({ isMenuOpen: false });
+    return action;
+  };
+
+  render() {
+    const { id, type, label, state, alignment, icon, options, selected } = this.props;
+    const { isMenuOpen } = this.state;
+
+    return (
+      <div
+        id={id}
+        className={[
+          'menu',
+          `menu--${alignment?.toLocaleLowerCase().replace('_', '-')}`,
+        ]
+          .filter((n) => n)
+          .join(' ')}
+      >
+        {type === 'ICON' ? (
+          <Button
+            type="icon"
+            icon={icon}
+            state={isMenuOpen ? 'selected' : ''}
+            isLoading= {state === 'LOADING'}
+            isDisabled= {state === 'DISABLED'}
+            action={() => {
+              this.setState({
+                isMenuOpen: !isMenuOpen,
+              });
+            }}
+          />
+        ) : (
+          <Button
+            type="primary"
+            label={label}
+            hasMultipleActions
+            isLoading= {state === 'LOADING'}
+            isDisabled= {state === 'DISABLED'}
+            action={() =>
+              this.setState({
+                isMenuOpen: !isMenuOpen,
+              })
+            }
+          />
+        )}
+        {(() => {
+          if (isMenuOpen)
+            return (
+              <div
+                className="floating-menu"
+                style={{
+                  position: 'absolute',
+                  zIndex: 99,
+                }}
+              >
+                <List
+                  options={options}
+                  selected={selected}
+                  direction={alignment?.includes('LEFT') ? 'RIGHT' : 'LEFT'}
+                  onCancellation={() => this.setState({ isMenuOpen: false })}
+                />
+              </div>
+            );
+          return null;
+        })()}
+      </div>
+    );
+  }
+}
---
+++ src/stories/actions/Menu.stories.tsx
@@ -0,0 +1,135 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { useArgs } from '@storybook/client-api'
+import { Menu } from '../../components/actions/menu/Menu'
+import * as ListStories from '../lists/List.stories'
+
+const meta = {
+  title: 'Example/Actions/Multiple Actions',
+  component: Menu,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Menu>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DropdownIcon: Story = {
+  args: {
+    id: 'dropdown-icon',
+    type: 'ICON',
+    icon: 'adjust',
+    state: 'DEFAULT',
+    options: { ...ListStories.FourOptionsList.args.options },
+    selected: 'OPTION_1',
+    alignment: 'BOTTOM_LEFT',
+  },
+  argTypes: {
+    type: { control: false },
+    label: { control: false },
+  },
+  render: (args) => {
+    const [argsState, updateArgs] = useArgs<{
+      selected: string
+    }>()
+
+    const onChange = (
+      e:
+        | React.MouseEvent<HTMLLIElement, MouseEvent>
+        | React.KeyboardEvent<HTMLLIElement>
+    ) => {
+      updateArgs({
+        selected: (e.target as HTMLInputElement).dataset.value,
+      })
+    }
+
+    return (
+      <Menu
+        {...args}
+        options={[
+          {
+            label: 'Option 1',
+            value: 'OPTION_1',
+            feature: null,
+            position: 0,
+            type: 'OPTION',
+            isActive: true,
+            isBlocked: false,
+            isNew: false,
+            children: [],
+            action: onChange,
+          },
+          {
+            label: 'Option 2',
+            value: 'OPTION_2',
+            feature: null,
+            position: 1,
+            type: 'OPTION',
+            isActive: true,
+            isBlocked: false,
+            isNew: false,
+            children: [],
+            action: onChange,
+          },
+          {
+            label: 'Option 3',
+            value: 'OPTION_3',
+            feature: null,
+            position: 2,
+            type: 'OPTION',
+            isActive: true,
+            isBlocked: false,
+            isNew: false,
+            children: [],
+            action: onChange,
+          },
+          {
+            label: 'Option 4',
+            value: 'OPTION_4',
+            feature: null,
+            position: 3,
+            type: 'OPTION',
+            isActive: true,
+            isBlocked: false,
+            isNew: false,
+            children: [],
+            action: onChange,
+          },
+        ]}
+        selected={argsState.selected}
+      />
+    )
+  },
+}
+
+export const MultipleActionsIconButton: Story = {
+  args: {
+    id: 'dropdown-icon',
+    type: 'ICON',
+    icon: 'ellipsis',
+    state: 'DEFAULT',
+    options: [ ...ListStories.FourOptionsList.args.options ],
+    alignment: 'BOTTOM_LEFT',
+  },
+  argTypes: {
+    type: { control: false },
+    label: { control: false },
+    selected: { control: false },
+  },
+}
+
+export const MultipleActionsButton: Story = {
+  args: {
+    id: 'dropdown-icon',
+    type: 'PRIMARY',
+    label: 'Run',
+    state: 'DEFAULT',
+    options: [ ...ListStories.FourOptionsList.args.options ],
+    alignment: 'BOTTOM_LEFT',
+  },
+  argTypes: {
+    type: { control: false },
+    icon: { control: false },
+    selected: { control: false },
+  },
+}
