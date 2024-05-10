f7fbae5a752bb12910542c3ed77bbe10a62df997
Aurélien Grimaud <a.grimaud@involt.io>
2024-05-11 00:07:36 +0200

    add: Accordion

---
+++ src/components/actions/accordion/Accordion.tsx
@@ -0,0 +1,80 @@
+import { Button } from '../button/Button';
+import { Chip } from '../../tags/chip/Chip'
+import { SectionTitle } from '../../assets/section-title/SectionTitle';
+import './accordion.scss';
+
+export interface AccordionProps {
+  label: string;
+  indicator?: string | number;
+  helper?: string;
+  isExpanded: boolean;
+  isBlocked?: boolean;
+  isNew?: boolean;
+  children?: React.ReactNode;
+  onAdd: React.MouseEventHandler<Element> & React.KeyboardEventHandler<Element>;
+  onEmpty: React.MouseEventHandler<Element> &
+    React.KeyboardEventHandler<Element>;
+}
+
+export const Accordion = (props: AccordionProps) => {
+  const {
+    label,
+    indicator,
+    helper,
+    isExpanded,
+    isBlocked = false,
+    isNew = false,
+    children,
+    onAdd,
+    onEmpty,
+  } = props;
+
+  return (
+    <div
+      role="row"
+      className={[
+        'accordion',
+        isExpanded ? 'accordion--expanded' : null,
+        isBlocked ? 'accordion--blocked' : null,
+      ]
+        .filter((n) => n)
+        .join(' ')}
+      tabIndex={-1}
+      onMouseDown={(e) => {
+        if (
+          (e.target as HTMLElement).dataset.feature === undefined &&
+          !isExpanded &&
+          !isBlocked
+        )
+          onAdd(e as React.MouseEvent<HTMLDivElement, MouseEvent>);
+      }}
+    >
+      <div className="accordion__row">
+        <div className="accordion__row__left-part">
+          <SectionTitle label={label} indicator={indicator} helper={helper} />
+        </div>
+        <div className="accordion__row__right-part">
+          {isExpanded ? (
+            <Button
+              type="icon"
+              icon="minus"
+              isDisabled={isBlocked}
+              isBlocked={isBlocked}
+              action={onEmpty}
+            />
+          ) : (
+            <Button
+              type="icon"
+              icon="plus"
+              isDisabled={isBlocked}
+              isBlocked={isBlocked}
+              action={onAdd}
+            />
+          )}
+          {isBlocked || isNew ? <Chip>{isNew ? 'New' : 'Pro'}</Chip> : null}
+        </div>
+      </div>
+      {isExpanded ? <div>{children}</div> : null}
+    </div>
+  );
+};
---
+++ src/stories/actions/Accordion.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { fn } from '@storybook/test'
+import { useArgs } from '@storybook/client-api'
+import { Accordion } from '../../components/actions/accordion/Accordion'
+import { Input } from '../../components/inputs/input/Input'
+import * as TitleStory from '../assets/SectionTitle.stories'
+import * as InputStory from '../inputs/Input.stories'
+
+const mock = fn()
+
+const meta = {
+  title: 'Example/Actions/Accordion',
+  component: Accordion,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Accordion>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const ExpandCollapseInput: Story = {
+  args: {
+    ...TitleStory.TitleWithHelper.args,
+    isExpanded: false,
+    isBlocked: false,
+    isNew: false,
+    children: <Input {...InputStory.ShortText.args} />,
+    onAdd: mock,
+    onEmpty: mock
+  },
+  argTypes: {
+    ...TitleStory.TitleWithHelper.argTypes,
+    children: { control: false },
+    onAdd: { control: false },
+    onEmpty: { control: false },
+  },
+  render: (args) => {
+    const [argsState, updateArgs] = useArgs<{
+      isExpanded: boolean
+    }>()
+
+    const onChange = () => {
+      updateArgs({
+        isExpanded: !argsState.isExpanded,
+      })
+    }
+
+    return (
+      <Accordion
+        {...args}
+        isExpanded={argsState.isExpanded}
+        onAdd={onChange}
+        onEmpty={onChange}
+      />
+    )
+  },
+}
