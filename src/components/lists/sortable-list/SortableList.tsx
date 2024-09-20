2ba310d4b5c636766abda1b0b4b2029572fb49df
@a-ng-d <a.grimaud@involt.io>
2024-09-21 00:04:33 +0200

    feat: add SortableList component

---
+++ src/components/lists/sortable-list/SortableList.tsx
@@ -0,0 +1,223 @@
+import React from 'react'
+import './sortable-list.scss'
+import { DraggableItem } from '../draggable-item/DraggableItem'
+
+interface SelectedColor {
+  id: string | undefined
+  position: number
+}
+
+interface HoveredColor extends SelectedColor {
+  hasGuideAbove: boolean
+  hasGuideBelow: boolean
+}
+
+export interface SortableListProps {
+  data: Array<{ [key: string]: string }>
+  primarySlot: Array<React.ReactNode>
+  secondarySlot?: Array<React.ReactNode>
+  actionsSlot?: Array<React.ReactNode>
+  isScrollable?: boolean
+  onChangeSortableList: (data: Array<{ [key: string]: string }>) => void
+}
+
+interface SortableListStates {
+  selectedElement: SelectedColor
+  hoveredElement: HoveredColor
+}
+
+export class SortableList extends React.Component<
+  SortableListProps,
+  SortableListStates
+> {
+  listRef: React.RefObject<HTMLUListElement>
+
+  static defaultProps: Partial<SortableListProps> = {
+    isScrollable: false,
+  }
+
+  constructor(props: SortableListProps) {
+    super(props)
+    this.state = {
+      selectedElement: {
+        id: undefined,
+        position: 0,
+      },
+      hoveredElement: {
+        id: undefined,
+        hasGuideAbove: false,
+        hasGuideBelow: false,
+        position: 0,
+      },
+    }
+    this.listRef = React.createRef()
+    this.handleClickOutside = this.handleClickOutside.bind(this)
+  }
+
+  // Lifecycle
+  componentDidMount = () =>
+    document.addEventListener('mousedown', this.handleClickOutside)
+
+  componentWillUnmount = () =>
+    document.removeEventListener('mousedown', this.handleClickOutside)
+
+  handleClickOutside = (e: Event) => {
+    if (this.listRef.current !== null)
+      if (
+        this.listRef &&
+        !this.listRef.current.contains(e.target as HTMLElement)
+      )
+        this.setState({
+          selectedElement: {
+            id: undefined,
+            position: 0,
+          },
+        })
+  }
+
+  // Handlers
+  orderHandler = () => {
+    const { onChangeSortableList, data } = this.props
+    const { selectedElement, hoveredElement } = this.state
+
+    const source: SelectedColor = selectedElement,
+      target: HoveredColor = hoveredElement,
+      duplicatedData = data.map((el) => el)
+
+    let position: number
+    const sourceIndex = duplicatedData.findIndex(
+      (item) => item.id === source.id
+    )
+
+    const [removedElement] = duplicatedData.splice(sourceIndex, 1)
+
+    if (target.hasGuideAbove && target.position > source.position) {
+      position = target.position - 1
+    } else if (target.hasGuideBelow && target.position > source.position) {
+      position = target.position
+    } else if (target.hasGuideAbove && target.position < source.position) {
+      position = target.position
+    } else if (target.hasGuideBelow && target.position < source.position) {
+      position = target.position + 1
+    } else {
+      position = target.position
+    }
+
+    duplicatedData.splice(position, 0, removedElement)
+
+    onChangeSortableList(duplicatedData)
+  }
+
+  selectionHandler: React.MouseEventHandler<HTMLLIElement> &
+    React.MouseEventHandler<Element> &
+    React.FocusEventHandler<HTMLInputElement> = (e) => {
+    const item = e.currentTarget as HTMLElement
+    const target = e.target as HTMLElement
+
+    if (
+      target.tagName === 'INPUT' ||
+      target.tagName === 'BUTTON' ||
+      target.tagName === 'TEXTAREA'
+    )
+      return
+
+    this.setState({
+      selectedElement: {
+        id: item.dataset.id,
+        position: parseFloat(item.dataset.position ?? '0'),
+      },
+    })
+  }
+
+  dragHandler = (
+    id: string | undefined,
+    hasGuideAbove: boolean,
+    hasGuideBelow: boolean,
+    position: number
+  ) => {
+    this.setState({
+      hoveredElement: {
+        id: id,
+        hasGuideAbove: hasGuideAbove,
+        hasGuideBelow: hasGuideBelow,
+        position: position,
+      },
+    })
+  }
+
+  dropOutsideHandler = (e: React.DragEvent<HTMLLIElement>) => {
+    const target = e.target,
+      parent: ParentNode =
+        (target as HTMLElement).parentNode ?? (target as HTMLElement),
+      scrollY: number = (parent.parentNode?.parentNode as HTMLElement)
+        .scrollTop,
+      parentRefTop: number = (parent as HTMLElement).offsetTop,
+      parentRefBottom: number =
+        parentRefTop + (parent as HTMLElement).clientHeight
+
+    if (e.pageY + scrollY < parentRefTop) this.orderHandler()
+    else if (e.pageY + scrollY > parentRefBottom) this.orderHandler()
+  }
+
+  removeHandler = (
+    e: React.MouseEvent<Element, MouseEvent> | React.KeyboardEvent<Element>
+  ) => {
+    const { onChangeSortableList, data } = this.props
+    const duplicatedData = data.map((el) => el)
+    let id: string | null
+    const element: HTMLElement | null = (
+      e.currentTarget as HTMLElement
+    ).closest('.draggable-item')
+
+    element !== null ? (id = element.getAttribute('data-id')) : (id = null)
+
+    onChangeSortableList(duplicatedData.filter((item) => item.id !== id))
+  }
+
+  // Render
+  render() {
+    const { data, primarySlot, secondarySlot, actionsSlot, isScrollable } =
+      this.props
+    const { selectedElement, hoveredElement } = this.state
+
+    return (
+      <ul
+        className={[
+          'sortable-list',
+          isScrollable && 'sortable-list--scrollable',
+        ]
+          .filter((n) => n)
+          .join(' ')}
+        ref={this.listRef}
+      >
+        {data.map((item, index) => (
+          <DraggableItem
+            key={item.id}
+            id={item.id}
+            index={index}
+            primarySlot={primarySlot[index]}
+            secondarySlot={secondarySlot ? secondarySlot[index] : undefined}
+            actionsSlot={actionsSlot ? actionsSlot[index] : undefined}
+            selected={selectedElement.id === item.id}
+            guideAbove={
+              hoveredElement.id === item.id
+                ? hoveredElement.hasGuideAbove
+                : false
+            }
+            guideBelow={
+              hoveredElement.id === item.id
+                ? hoveredElement.hasGuideBelow
+                : false
+            }
+            onCancellationSelection={this.selectionHandler}
+            onChangeOrder={this.orderHandler}
+            onRemove={(e) => this.removeHandler(e)}
+            onChangeSelection={this.selectionHandler}
+            onDragChange={this.dragHandler}
+            onDropOutside={this.orderHandler}
+          />
+        ))}
+      </ul>
+    )
+  }
+}
---
+++ src/stories/lists/SortableList.stories.tsx
@@ -0,0 +1,185 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { useArgs } from '@storybook/client-api'
+import { fn } from '@storybook/test'
+import { SortableList } from '../../components/lists/sortable-list/SortableList'
+import { Input } from '../../components/inputs/input/Input'
+import { Button } from '../../components/actions/button/Button'
+import { FormItem } from '../../components/slots/form-item/FormItem'
+
+const mock = fn()
+
+const meta = {
+  title: 'Example/Lists/SortableList',
+  component: SortableList,
+  decorators: [
+    (Story) => (
+      <div style={{ width: '296px', height: '148px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof SortableList>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const SimpleColors: Story = {
+  args: {
+    data: [
+      {
+        id: '000000',
+        color: '#FF0000',
+      },
+      {
+        id: '111111',
+        color: '#00FF00',
+      },
+      {
+        id: '222222',
+        color: '#0000FF',
+      },
+      {
+        id: '333333',
+        color: '#000000',
+      },
+      {
+        id: '444444',
+        color: '#FFFFFF',
+      },
+    ],
+    primarySlot: [<div></div>],
+    actionsSlot: [<div></div>],
+    isScrollable: false,
+    onChangeSortableList: mock,
+  },
+  argTypes: {
+    primarySlot: { control: false },
+    secondarySlot: { control: false },
+    actionsSlot: { control: false },
+    onChangeSortableList: { control: false },
+  },
+  render: (args) => {
+    const [argsState, updateArgs] = useArgs<{
+      data: Array<{ [key: string]: string }>
+    }>()
+
+    const onChange = (e: Array<{ [key: string]: string }>) => {
+      updateArgs({
+        data: e,
+      })
+      args.onChangeSortableList?.(e)
+    }
+
+    return (
+      <SortableList
+        {...args}
+        primarySlot={argsState.data.map((item) => (
+          <Input
+            type="COLOR"
+            value={item.color}
+          />
+        ))}
+        actionsSlot={argsState.data.map(() => (
+          <Button
+            type="icon"
+            icon="visible"
+            action={mock}
+          />
+        ))}
+        onChangeSortableList={onChange}
+      />
+    )
+  },
+}
+
+export const RichColors: Story = {
+  args: {
+    data: [
+      {
+        id: '000000',
+        color: '#FF0000',
+        description: 'Some description',
+      },
+      {
+        id: '111111',
+        color: '#00FF00',
+        description: 'Some description',
+      },
+      {
+        id: '222222',
+        color: '#0000FF',
+        description: 'Some description',
+      },
+      {
+        id: '333333',
+        color: '#000000',
+        description: '',
+      },
+      {
+        id: '444444',
+        color: '#FFFFFF',
+        description: 'Some description',
+      },
+      {
+        id: '555555',
+        color: '#123456',
+        description: '',
+      },
+      {
+        id: '666666',
+        color: '#654321',
+        description: 'Some description',
+      },
+    ],
+    primarySlot: [<div></div>],
+    isScrollable: false,
+    onChangeSortableList: mock,
+  },
+  argTypes: {
+    primarySlot: { control: false },
+    secondarySlot: { control: false },
+    actionsSlot: { control: false },
+    onChangeSortableList: { control: false },
+  },
+  render: (args) => {
+    const [argsState, updateArgs] = useArgs<{
+      data: Array<{ [key: string]: string }>
+    }>()
+
+    const onChange = (e: Array<{ [key: string]: string }>) => {
+      updateArgs({
+        data: e,
+      })
+      args.onChangeSortableList?.(e)
+    }
+
+    return (
+      <SortableList
+        {...args}
+        primarySlot={argsState.data.map((item) => (
+          <Input
+            type="COLOR"
+            value={item.color}
+          />
+        ))}
+        secondarySlot={argsState.data.map((item) => (
+          <FormItem
+            label="Description"
+            id="type-description"
+          >
+            <Input
+              id="type-description"
+              type="LONG_TEXT"
+              placeholder="Type something"
+              value={item.description}
+            />
+          </FormItem>
+        ))}
+        onChangeSortableList={onChange}
+      />
+    )
+  },
+}
