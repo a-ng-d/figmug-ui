ad886c225109a71a05d44c34fe0c163d45962059
@a-ng-d <a.grimaud@involt.io>
2024-09-26 22:24:12 +0200

    refactor: define new props for SimpleItem

--- src/components/slots/simple-item/SimpleItem.tsx
+++ src/components/slots/simple-item/SimpleItem.tsx
@@ -6,27 +6,58 @@ export interface SimpleItemProps {
   leftPartSlot: React.ReactNode
   rightPartSlot?: React.ReactNode
   isCompact?: boolean
+  isListItem?: boolean
+  alignment?: 'DEFAULT' | 'CENTER' | 'BASELINE'
 }
 
 export class SimpleItem extends React.Component<SimpleItemProps> {
   static defaultProps: Partial<SimpleItemProps> = {
     isCompact: false,
+    isListItem: true,
+    alignment: 'DEFAULT',
   }
 
   // Render
   render() {
-    const { id, leftPartSlot, rightPartSlot, isCompact } = this.props
+    const {
+      id,
+      leftPartSlot,
+      rightPartSlot,
+      isCompact,
+      isListItem,
+      alignment,
+    } = this.props
 
+    if (isListItem)
+      return (
+        <li
+          data-id={id}
+          className={[
+            'simple-item',
+            isCompact && 'simple-item__compact',
+            `${alignment?.toLowerCase()}`,
+          ]
+            .filter((n) => n)
+            .join(' ')}
+        >
+          <div className="simple-item__left-part">{leftPartSlot}</div>
+          <div className="simple-item__right-part">{rightPartSlot}</div>
+        </li>
+      )
     return (
-      <li
+      <div
         data-id={id}
-        className={['simple-item', isCompact && 'simple-item__compact']
+        className={[
+          'simple-item',
+          isCompact && 'simple-item__compact',
+          `${alignment?.toLowerCase()}`,
+        ]
           .filter((n) => n)
           .join(' ')}
       >
         <div className="simple-item__left-part">{leftPartSlot}</div>
         <div className="simple-item__right-part">{rightPartSlot}</div>
-      </li>
+      </div>
     )
   }
 }
