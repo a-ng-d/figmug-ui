4b8bfc41cd5075a285487006dca30b2673b5d84d
@a-ng-d <a.grimaud@involt.io>
2024-11-25 18:42:28 +0100

    feat: enhance Tooltip component with dynamic positioning

--- src/components/tags/tooltip/Tooltip.tsx
+++ src/components/tags/tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import texts from '../../../styles/texts.module.scss'
 import './tooltip.scss'
 
@@ -8,10 +8,30 @@ export interface TooltipProps {
 
 export const Tooltip = (props: TooltipProps) => {
   const { children } = props
+  const tooltipRef = useRef<HTMLDivElement>(null)
+  const [shift, setShift] = React.useState(0)
+
+  useEffect(() => {
+    const tooltipElement = tooltipRef.current
+    if (tooltipElement) {
+      const rect = tooltipElement.getBoundingClientRect()
+      if (rect.x < 0) setShift(-rect.x + 8)
+      if (rect.x + rect.width > window.innerWidth)
+        setShift(window.innerWidth - rect.x - rect.width - 8)
+    }
+  }, [])
 
   return (
-    <div className={['tooltip', 'recharged'].filter((n) => n).join(' ')}>
-      <div className="tooltip__block">
+    <div
+      className={['tooltip', 'recharged'].filter((n) => n).join(' ')}
+      ref={tooltipRef}
+    >
+      <div
+        className="tooltip__block"
+        style={{
+          transform: `translateX(${shift}px)`,
+        }}
+      >
         <div
           className={['tooltip__text', texts.type, 'type']
             .filter((n) => n)
