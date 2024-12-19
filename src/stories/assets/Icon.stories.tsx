b760d623695890054f15c2ebdcde9266fa0d2509
@a-ng-d <a.grimaud@involt.io>
2024-12-19 13:00:26 +0100

    refactor: display all the icons as a grid

--- src/stories/assets/Icon.stories.tsx
+++ src/stories/assets/Icon.stories.tsx
@@ -1,13 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Icon } from '../../components/assets/icon/Icon'
 import { IconList } from '../../types/icon.types'
+import texts from '../../styles/texts.module.scss'
+
+const width = 960
 
 const icons: Array<IconList> = [
   'adjust',
   'angle',
   'alert',
+  'align-horizontal-centers',
+  'align-left',
+  'align-right',
+  'align-top',
+  'align-vertical-centers',
   'arrow-left-right',
-  'up-down',
   'auto-layout-horizontal',
   'auto-layout-vertical',
   'back',
@@ -42,11 +49,6 @@ const icons: Array<IconList> = [
   'instance',
   'key',
   'layout-align-bottom',
-  'align-horizontal-centers',
-  'align-left',
-  'align-right',
-  'align-top',
-  'align-vertical-centers',
   'layout-grid-columns',
   'layout-grid-rows',
   'layout-grid-uniform',
@@ -92,6 +94,7 @@ const icons: Array<IconList> = [
   'tidy-up-list-vertical',
   'timer',
   'trash',
+  'up-down',
   'user',
   'vertical-padding',
   'visible',
@@ -102,6 +105,20 @@ const icons: Array<IconList> = [
 const meta = {
   title: 'Example/Assets/Icon',
   component: Icon,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          display: 'flex',
+          maxWidth: `${width}px`,
+          flexWrap: 'wrap',
+          gap: `${width / 80}px`,
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
   parameters: {
     layout: 'centered',
   },
@@ -116,14 +133,43 @@ type Story = StoryObj<typeof meta>
 export const Pictogram: Story = {
   args: {
     type: 'PICTO',
-    iconName: 'adjust',
     iconLetter: undefined,
     iconColor: 'var(--figma-color-icon)',
   },
   argTypes: {
     type: { control: false },
-    iconName: { control: 'select', options: icons },
+    iconName: { control: false },
     iconLetter: { control: false },
+  },
+  render: (args) => {
+    return (
+      <>
+        {icons.map((icon) => (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: `${width / 80}px`,
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: `${width / 8}px`,
+            }}
+          >
+            <Icon
+              type={args.type}
+              iconName={icon}
+              iconColor={args.iconColor}
+            />
+            <span
+              className={`type ${texts['type']}`}
+              style={{ textAlign: 'center' }}
+            >
+              {icon}
+            </span>
+          </div>
+        ))}
+      </>
+    )
   },
 }
 
@@ -137,5 +183,14 @@ export const Letter: Story = {
   argTypes: {
     type: { control: false },
     iconName: { control: false },
+  },
+  render: (args) => {
+    return (
+      <Icon
+        type={args.type}
+        iconLetter={args.iconLetter}
+        iconColor={args.iconColor}
+      />
+    )
   },
 }
