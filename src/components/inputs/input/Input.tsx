13064601d2cf0f9d7e5af2234749c5fc1e261f24
@a-ng-d <a.grimaud@involt.io>
2025-01-20 15:09:10 +0100

    refactor: change inputRef and textareaRef to public in Input component

--- src/components/inputs/input/Input.tsx
+++ src/components/inputs/input/Input.tsx
@@ -41,8 +41,8 @@ export interface InputStates {
 }
 
 export class Input extends React.Component<InputProps, InputStates> {
-  private inputRef: React.RefObject<HTMLInputElement>
-  private textareaRef: React.RefObject<HTMLTextAreaElement>
+  inputRef: React.RefObject<HTMLInputElement>
+  textareaRef: React.RefObject<HTMLTextAreaElement>
   private startValue: string
 
   static defaultProps: Partial<InputProps> = {
