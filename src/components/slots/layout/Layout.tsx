05ba21668d83801f1dd0bcb5b471abda7929802e
@a-ng-d <a.grimaud@involt.io>
2025-02-21 00:58:24 +0100

    refactor: simplify Layout component by consolidating slot props and updating layout styles

--- src/components/slots/layout/Layout.tsx
+++ src/components/slots/layout/Layout.tsx
@@ -2,90 +2,44 @@ import './Layout.scss'
 
 export type LayoutProps = {
   id: string
-  direction?: 'VERTICAL' | 'HORIZONTAL'
-  singleSlot: {
-    nodes: React.ReactElement
+  column: Array<{
+    node: React.ReactElement
     type: 'LIST' | 'DISTRIBUTED' | 'CENTERED'
     hasPadding?: boolean
-  }
-  leftSlot?: {
-    nodes: React.ReactElement
-    type: 'LIST' | 'DISTRIBUTED' | 'CENTERED'
-    hasPadding?: boolean
-  }
-  rightSlot?: {
-    nodes: React.ReactElement
-    type: 'NORMAL' | 'LIST' | 'DISTRIBUTED' | 'CENTERED'
-    hasPadding?: boolean
-  }
+  }>
   isFullHeight?: boolean
 }
 
 const Layout = (props: LayoutProps) => {
-  const {
-    id,
-    direction = 'VERTICAL',
-    singleSlot,
-    leftSlot,
-    rightSlot,
-    isFullHeight = false,
-  } = props
+  const { id, column, isFullHeight = false } = props
 
   return (
     <div
       id={id}
-      className={[
-        'layout',
-        direction === 'HORIZONTAL' && 'layout--horizontal',
-        isFullHeight && 'layout--full-height',
-      ]
+      className={['layout', isFullHeight && 'layout--full-height']
         .filter((n) => n)
         .join(' ')}
       role="layout"
     >
-      {leftSlot && (
-        <div
-          className={[
-            'layout__block',
-            leftSlot.type === 'LIST' && 'layout__block--list',
-            leftSlot.type === 'DISTRIBUTED' && 'layout__block--distributed',
-            leftSlot.type === 'CENTERED' && 'layout__block--centered',
-            !leftSlot.hasPadding && 'layout__block--no-padding',
-          ]
-            .filter((n) => n)
-            .join(' ')}
-        >
-          {leftSlot.nodes}
-        </div>
+      {column.map(
+        (item, index) =>
+          item.node !== undefined && (
+            <div
+              key={index}
+              className={[
+                'layout__block',
+                item.type === 'LIST' && 'layout__block--list',
+                item.type === 'DISTRIBUTED' && 'layout__block--distributed',
+                item.type === 'CENTERED' && 'layout__block--centered',
+                !item.hasPadding && 'layout__block--no-padding',
+              ]
+                .filter((n) => n)
+                .join(' ')}
+            >
+              {item.node}
+            </div>
+          )
       )}
-      {rightSlot && (
-        <div
-          className={[
-            'layout__block',
-            rightSlot.type === 'LIST' && 'layout__block--list',
-            rightSlot.type === 'DISTRIBUTED' && 'layout__block--distributed',
-            rightSlot.type === 'CENTERED' && 'layout__block--centered',
-            !rightSlot.hasPadding && 'layout__block--no-padding',
-          ]
-            .filter((n) => n)
-            .join(' ')}
-        >
-          {rightSlot.nodes}
-        </div>
-      )}
-      <div
-        className={[
-          'layout__block',
-          singleSlot.type === 'LIST' && 'layout__block--list',
-          singleSlot.type === 'DISTRIBUTED' && 'layout__block--distributed',
-          singleSlot.type === 'CENTERED' && 'layout__block--centered',
-          !singleSlot.hasPadding && 'layout__block--no-padding',
-        ]
-          .filter((n) => n)
-          .join(' ')}
-      >
-        {singleSlot.nodes}
-      </div>
     </div>
   )
 }
