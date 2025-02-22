16382019bdadcdca4e4d2fc12d4841834df558c5
@a-ng-d <a.grimaud@involt.io>
2025-02-23 00:15:51 +0100

    feat: add Drawer component for flexible layout with customizable size and direction

---
+++ src/components/slots/drawer/Drawer.tsx
@@ -0,0 +1,212 @@
+import React from 'react'
+import './drawer.scss'
+
+export type DrawerProps = {
+  id: string
+  direction: 'VERTICAL' | 'HORIZONTAL'
+  pin: 'TOP' | 'BOTTOM' | 'LEFT' | 'RIGHT'
+  defaultSize: {
+    value?: number
+    unit: 'PIXEL' | 'PERCENT' | 'AUTO'
+  }
+  maxSize: {
+    value?: number
+    unit: 'PIXEL' | 'PERCENT' | 'AUTO'
+  }
+  minSize: {
+    value?: number
+    unit: 'PIXEL' | 'PERCENT' | 'AUTO'
+  }
+  border?: Array<'TOP' | 'LEFT' | 'BOTTOM' | 'RIGHT'>
+  children?: React.ReactNode
+  onCollapse?: () => void
+  onExpand?: () => void
+}
+
+export type DrawerState = {
+  isDrawerCollapsed: boolean
+  drawerSize: {
+    value?: number
+    unit: 'PIXEL' | 'PERCENT' | 'AUTO'
+  }
+}
+
+export default class Drawer extends React.Component<DrawerProps, DrawerState> {
+  drawerRef: React.RefObject<HTMLDivElement>
+
+  constructor(props: DrawerProps) {
+    super(props)
+    this.state = {
+      isDrawerCollapsed: false,
+      drawerSize: {
+        value: props.defaultSize.value,
+        unit: props.defaultSize.unit,
+      },
+    }
+    this.drawerRef = React.createRef()
+  }
+
+  // Lifecycle
+  componentDidUpdate = (
+    _: Readonly<DrawerProps>,
+    prevState: Readonly<DrawerState>
+  ) => {
+    const { onCollapse, onExpand } = this.props
+    const { isDrawerCollapsed } = this.state
+
+    if (isDrawerCollapsed && !prevState.isDrawerCollapsed)
+      onCollapse && onCollapse()
+    if (!isDrawerCollapsed && prevState.isDrawerCollapsed)
+      onExpand && onExpand()
+  }
+
+  // Handlers
+  clickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+    const { defaultSize, maxSize } = this.props
+    const { drawerSize } = this.state
+
+    document.body.style.cursor = ''
+    document.removeEventListener('mousemove', this.onDrag)
+
+    if (e.detail === 2)
+      this.setState({
+        drawerSize: drawerSize === defaultSize ? maxSize : defaultSize,
+        isDrawerCollapsed: false,
+      })
+  }
+
+  // Direct Actions
+  setBorder = (
+    orientation: Array<'TOP' | 'LEFT' | 'BOTTOM' | 'RIGHT'> | undefined
+  ) => {
+    const property =
+      '1px solid var(--figma-color-border)' as React.CSSProperties
+    const styles: { [key: string]: React.CSSProperties } = {}
+
+    if (!orientation) return styles
+    orientation.forEach((entry) => {
+      if (entry === 'TOP') styles.borderTop = property
+      if (entry === 'LEFT') styles.borderLeft = property
+      if (entry === 'BOTTOM') styles.borderBottom = property
+      if (entry === 'RIGHT') styles.borderRight = property
+    })
+    return styles
+  }
+
+  setUnit = (unit: 'PIXEL' | 'PERCENT' | 'AUTO') => {
+    return unit === 'PIXEL' ? 'px' : unit === 'PERCENT' ? '%' : 'auto'
+  }
+
+  expandDrawer = () => {
+    const { defaultSize } = this.props
+
+    this.setState({
+      drawerSize: defaultSize,
+      isDrawerCollapsed: false,
+    })
+  }
+
+  collapseDrawer = () => {
+    const { minSize } = this.props
+
+    this.setState({
+      drawerSize: minSize,
+      isDrawerCollapsed: true,
+    })
+  }
+
+  onGrab = () => {
+    document.body.style.cursor = 'ns-resize'
+    document.addEventListener('mousemove', this.onDrag)
+  }
+
+  onDrag = (e: MouseEvent) => {
+    const { direction, pin, minSize } = this.props
+    const { drawerRef } = this
+    const { clientX, clientY } = e
+
+    let anchor = 0
+    if (pin === 'TOP')
+      anchor = drawerRef.current
+        ? drawerRef.current.getBoundingClientRect().top
+        : 0
+    if (pin === 'RIGHT')
+      anchor = drawerRef.current
+        ? drawerRef.current.getBoundingClientRect().right
+        : 0
+    if (pin === 'BOTTOM')
+      anchor = drawerRef.current
+        ? drawerRef.current.getBoundingClientRect().bottom
+        : 0
+    if (pin === 'LEFT')
+      anchor = drawerRef.current
+        ? drawerRef.current.getBoundingClientRect().left
+        : 0
+
+    const offset = direction === 'VERTICAL' ? clientY : clientX
+
+    console.log(anchor, offset)
+
+    const delta = Math.abs(anchor - offset)
+
+    this.setState({
+      drawerSize: {
+        value: delta,
+        unit: 'PIXEL',
+      },
+      isDrawerCollapsed: delta <= (minSize.value ?? 0),
+    })
+
+    document.body.style.cursor =
+      direction === 'VERTICAL' ? 'ns-resize' : 'ew-resize'
+    document.addEventListener('mouseup', () => {
+      document.body.style.cursor = ''
+      document.removeEventListener('mousemove', this.onDrag)
+    })
+  }
+
+  // Render
+  render() {
+    const { id, pin, children, direction, border, maxSize, minSize } =
+      this.props
+    const { drawerSize } = this.state
+
+    return (
+      <div
+        id={id}
+        style={{
+          ...this.setBorder(border),
+          ...(direction === 'VERTICAL'
+            ? {
+                height: `${drawerSize.value}${this.setUnit(drawerSize.unit)}`,
+                maxHeight: `${maxSize.value}${this.setUnit(maxSize.unit)}`,
+                minHeight: `${minSize.value}${this.setUnit(minSize.unit)}`,
+              }
+            : {
+                width: `${drawerSize.value}${this.setUnit(drawerSize.unit)}`,
+                maxWidth: `${maxSize.value}${this.setUnit(maxSize.unit)}`,
+                minWidth: `${minSize.value}${this.setUnit(minSize.unit)}`,
+              }),
+        }}
+        className={['drawer'].filter((n) => n).join(' ')}
+        role="layout"
+        ref={this.drawerRef}
+      >
+        <div
+          className={[
+            'drawer__knob',
+            pin === 'TOP' && 'drawer__knob--top',
+            pin === 'LEFT' && 'drawer__knob--left',
+            pin === 'BOTTOM' && 'drawer__knob--bottom',
+            pin === 'RIGHT' && 'drawer__knob--right',
+          ]
+            .filter((n) => n)
+            .join(' ')}
+          onMouseDown={this.onGrab}
+          onClick={this.clickHandler}
+        />
+        {children}
+      </div>
+    )
+  }
+}
---
+++ src/stories/slots/Drawer.stories.tsx
@@ -0,0 +1,99 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Drawer from '@components/slots/drawer/Drawer'
+import Button from '@components/actions/button/Button'
+import { useRef } from 'react'
+import { useArgs } from '@storybook/client-api'
+
+const meta = {
+  title: 'Components/Slots/Drawer',
+  component: Drawer,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Drawer>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    id: 'simple-drawer',
+    direction: 'VERTICAL',
+    pin: 'BOTTOM',
+    border: ['TOP'],
+    defaultSize: {
+      value: 100,
+      unit: 'PIXEL',
+    },
+    maxSize: {
+      value: 300,
+      unit: 'PIXEL',
+    },
+    minSize: {
+      value: 40,
+      unit: 'PIXEL',
+    },
+  },
+  render: (args) => {
+    const drawerRef = useRef<Drawer>(null)
+    const [argsState, updateArgs] = useArgs<{
+      isCollapsed: boolean
+    }>()
+
+    const toggleDrawer = () => {
+      if (drawerRef.current && argsState.isCollapsed)
+        drawerRef.current.expandDrawer()
+      else if (drawerRef.current && !argsState.isCollapsed)
+        drawerRef.current.collapseDrawer()
+    }
+
+    const onExpand = () => {
+      updateArgs({
+        isCollapsed: false,
+      })
+    }
+
+    const onCollapse = () => {
+      updateArgs({
+        isCollapsed: true,
+      })
+    }
+
+    return (
+      <div
+        style={{
+          width: '400px',
+          height: '400px',
+          backgroundColor: 'white',
+          display: 'flex',
+          flexDirection: args.direction === 'VERTICAL' ? 'column' : 'row',
+          justifyContent:
+            args.pin === 'TOP' || args.pin === 'LEFT'
+              ? 'flex-start'
+              : 'flex-end',
+        }}
+      >
+        <Drawer
+          ref={drawerRef}
+          {...args}
+          children={
+            <Button
+              type="icon"
+              icon={argsState.isCollapsed ? 'upward' : 'downward'}
+              action={toggleDrawer}
+            />
+          }
+          border={[
+            args.pin === 'TOP'
+              ? 'BOTTOM'
+              : args.pin === 'RIGHT'
+                ? 'LEFT'
+                : 'TOP',
+          ]}
+          onExpand={onExpand}
+          onCollapse={onCollapse}
+        />
+      </div>
+    )
+  },
+}
