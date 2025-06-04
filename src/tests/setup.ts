6d3601185ce49871c911502f2f78357c85444da4
@a-ng-d <a.grimaud@involt.io>
2025-06-04 21:34:16 +0200

    fix: update test setup file path and add cleanup logic for tests

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom'
 import { cleanup } from '@testing-library/react'
 import { afterEach } from 'vitest'
 
-// hooks are reset before each suite
+// Hooks are reset before each suite
 afterEach(() => {
   cleanup()
 })
