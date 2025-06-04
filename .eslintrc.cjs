0cd4266ccd8f2e08fbd2754be34d73c570aed93d
@a-ng-d <a.grimaud@involt.io>
2025-06-05 00:59:26 +0200

    chore: update ESLint configuration to include 'import' plugin and add import/order rule for better module organization

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,11 +10,68 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        pathGroups: [
+          {
+            pattern: '@a_ng_d/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@storybook/**',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@tps/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@components/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@stories/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@styles/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '*.scss',
+            group: 'sibling',
+            position: 'after',
+          },
+        ],
+        'newlines-between': 'never',
+        alphabetize: {
+          order: 'desc',
+          caseInsensitive: true,
+        },
+      },
     ],
     curly: ['warn', 'multi'],
     'prefer-const': 'warn',
