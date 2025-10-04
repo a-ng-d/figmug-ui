8311c220bd1cd6680e92f9efab988a7cdb6076d3
@a-ng-d <a.grimaud@involt.io>
2025-10-05 00:37:00 +0200

    feat: reorganize Penpot styles and components for dialogs and notifications

---
+++ terrazzo/penpot/components/terrazzo.consent.js
@@ -0,0 +1,39 @@
+import css from '@terrazzo/plugin-css'
+import { defineConfig } from '@terrazzo/cli'
+
+export default defineConfig({
+  name: 'global',
+  tokens: [
+    './tokens/globals/color.json',
+    './tokens/globals/typography.json',
+    './tokens/globals/spacing.json',
+    './tokens/globals/effect.json',
+    './tokens/globals/radius.json',
+    './tokens/platforms/penpot/color.json',
+    './tokens/platforms/penpot/typography.json',
+    './tokens/platforms/penpot/icon.json',
+    './tokens/platforms/penpot/components/consent.json',
+  ],
+  outDir: './src/components/dialogs/consent/',
+  plugins: [
+    css({
+      filename: 'styles/penpot.scss',
+      exclude: [
+        'penpot.color.*',
+        'font.*',
+        'size.*',
+        'shadow.*',
+        'border.*',
+        'grey.*',
+        'elevation.*',
+        'icon.*',
+      ],
+      baseSelector: ':root[data-theme="penpot"]',
+    }),
+  ],
+  lint: {
+    rules: {
+      // my lint rules
+    },
+  },
+})
