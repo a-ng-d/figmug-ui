d3e99a5de88e64e467fc11e2c3cf9754f4186ba9
@a-ng-d <a.grimaud@involt.io>
2026-01-15 18:27:24 +0100

    fix: update storybookUrl to use process.env instead of import.meta.env

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     storybookTest({
       configDir: '.storybook',
       storybookScript: 'npm run storybook -- --no-open',
-      storybookUrl: import.meta.env.STORYBOOK_URL,
+      storybookUrl: process.env.STORYBOOK_URL,
     }),
   ],
   test: {
